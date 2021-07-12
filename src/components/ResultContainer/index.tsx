f8f4e3d5d03576646e0cb871ecfd9a50a683d4d6
Wardes Thiago Costa <wardes.costa@gmail.com>
2021-07-12 18:11:41 -0400

    Fixing git conflicts

--- src/components/ResultContainer/index.tsx
+++ src/components/ResultContainer/index.tsx
@@ -1,27 +1,10 @@
 import './styles.css';
 
 import { Profile } from 'types/profile';
-<<<<<<< HEAD
-=======
-
->>>>>>> refatoctoring
 import ResultCard from 'components/ResultCard';
 
 type Props = {
   profile?: Profile;
-<<<<<<< HEAD
-}
-
-const ResultContainer = ( {profile }: Props) => {
-  return (
-    <div className="result-container">
-      <div className="profile-image">
-        <p>Imagem</p>
-        <img src={profile?.avatar_url} alt={profile?.name} />
-      </div>
-      <div className="profile-records">
-        <h3 className="text-primary">Informações</h3>
-=======
 };
 const ResultContainer = ({ profile }: Props) => {
   return (
@@ -31,7 +14,6 @@ const ResultContainer = ({ profile }: Props) => {
       </div>
       <div className="profile-records">
         <h3>Informações</h3>
->>>>>>> refatoctoring
         <ResultCard title="Perfil" description={profile?.url} link={true} />
         <ResultCard title="Número" description={profile?.followers} />
         <ResultCard title="Localidade" description={profile?.location} />
